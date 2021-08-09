9fe8d4c282c770be739b74ffe36413cb9f90cf3f
Vladislav Kuzmich <vkuzmich@aquivalabs.com>
2021-08-09 13:59:20 +0300

    node styling

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
-import ReactFlow from "react-flow-renderer";
+import ReactFlow, {Background} from "react-flow-renderer";
 function App() {
   const elements = [
-    { id: "1", data: { label: "Node 1" }, position: { x: 250, y: 5 } },
-    { id: "3", data: { label: "Node 3" }, position: { x: 250, y: 5 } },
-    { id: "4", data: { label: "Node 4" }, position: { x: 250, y: 5 } },
+    { id: "1",
+      type: "input",
+      data: { label: "Node 1" },
+      position: { x: 250, y: 200 },
+      sourcePosition: 'right',
+    },
 
     {
       id: "2",
+      type: "output",
       data: { label: <div>Node 2</div> },
-      position: { x: 100, y: 100 },
+      position: { x: 550, y: 200 },
+      targetPosition: 'left',
     },
-    { id: "e1-2", source: "1", target: "2", animated: true },
-    { id: "e3-2", source: "3", target: "2", animated: false },
-    { id: "e2-4", source: "4", target: "2", animated: false },
+    { id: "e1-2", source: "1", target: "2", animated: true, label: "label", labelShowBg: false },
   ];
   return (
     <div style={{ width: "100%", height: "100vh", backgroundColor: "#ccc" }}>
-      <ReactFlow elements={elements} />;
+      <ReactFlow elements={elements}>
+        <Background
+            variant="dots"
+            gap={20}
+            size={1}
+        />
+      </ReactFlow>
     </div>
   );
 }
