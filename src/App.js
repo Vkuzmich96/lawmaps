6b61df4a7fb9bf5608a5b8c3577cf01c1fb7e075
pavelkolechits <75511210+pavelkolechits@users.noreply.github.com>
2021-08-09 11:13:18 +0300

    init commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
-
+import ReactFlow from "react-flow-renderer";
 function App() {
+  const elements = [
+    { id: "1", data: { label: "Node 1" }, position: { x: 250, y: 5 } },
+    { id: "3", data: { label: "Node 3" }, position: { x: 250, y: 5 } },
+    { id: "4", data: { label: "Node 4" }, position: { x: 250, y: 5 } },
+
+    {
+      id: "2",
+      data: { label: <div>Node 2</div> },
+      position: { x: 100, y: 100 },
+    },
+    { id: "e1-2", source: "1", target: "2", animated: true },
+    { id: "e3-2", source: "3", target: "2", animated: false },
+    { id: "e2-4", source: "4", target: "2", animated: false },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "100%", height: "100vh", backgroundColor: "#ccc" }}>
+      <ReactFlow elements={elements} />;
     </div>
   );
 }
